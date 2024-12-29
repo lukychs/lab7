fcbec4306094b64adc1648ae218c15836ec944d2
Alex <Alex@DESKTOP-7B8N80I>
2024-12-30 00:15:43 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CommentAttribute : Attribute
+    {
+        public string Comment { get; }
+        public CommentAttribute(string comment)
+        {
+            Comment = comment;
+        }
+    }
+
+    public enum eClassificationAnimal
+    {
+        Herbivores,
+        Carnivores,
+        Omnivores
+    }
+
+    public enum eFavouriteFood
+    {
+        Meat,
+        Plants,
+        Everything
+    }
+
+    [Comment("Class Animal")]
+    public abstract class Animal
+    {
+        public abstract string Country { get; set; }
+        public abstract bool HideFromOtherAnimals { get; set; }
+        public abstract string Name { get; set; }
+        public abstract string WhatAnimal { get; set; }
+
+        public Animal(string Country, bool HideFromOtherAnimals, string Name, string WhatAnimal)
+        {
+            this.Country = Country;
+            this.HideFromOtherAnimals = HideFromOtherAnimals;
+            this.Name = Name;
+            this.WhatAnimal = WhatAnimal;
+        }
+
+        public abstract eClassificationAnimal GetClassificationAnimal();
+        public abstract eFavouriteFood GetFavouriteFood();
+        public abstract string SayHello();
+
+        public void Deconstruct(out string country, out bool hide, out string name, out string whatAnimal)
+        {
+            country = Country;
+            hide = HideFromOtherAnimals;
+            name = Name;
+            whatAnimal = WhatAnimal;
+        }
+
+    }
+
+    [Comment("Class Cow")]
+    public class Cow : Animal
+    {
+        public string country;
+        public bool hide;
+        public string name;
+        public string whatanimal;
+
+        public override string Country { get; set; }
+        public override bool HideFromOtherAnimals { get; set; }
+        public override string Name { get; set; }
+        public override string WhatAnimal { get; set; }
+
+        public Cow(string country, bool hide, string name, string whatanimal, string food) : base(country, hide, name, whatanimal) { }
+
+        public override eFavouriteFood GetFavouriteFood()
+        {
+            return eFavouriteFood.Meat;
+
+        }
+
+        public override eClassificationAnimal GetClassificationAnimal()
+        {
+            return eClassificationAnimal.Herbivores;
+        }
+
+        public override string SayHello()
+        {
+            return "a";
+        }
+
+    }
+
+    [Comment("Class Lion")]
+    public class Lion : Animal
+    {
+        public string country;
+        public bool hide;
+        public string name;
+        public string whatanimal;
+
+        public override string Country { get; set; }
+        public override bool HideFromOtherAnimals { get; set; }
+        public override string Name { get; set; }
+        public override string WhatAnimal { get; set; }
+
+        public Lion(string country, bool hide, string name, string whatanimal, string food) : base(country, hide, name, whatanimal) { }
+
+        public override eFavouriteFood GetFavouriteFood()
+        {
+            return eFavouriteFood.Plants;
+
+        }
+
+        public override eClassificationAnimal GetClassificationAnimal()
+        {
+            return eClassificationAnimal.Carnivores;
+        }
+
+        public override string SayHello()
+        {
+            return "b";
+        }
+
+    }
+
+    [Comment("Class Pig")]
+    public class Pig : Animal
+    {
+        public string country;
+        public bool hide;
+        public string name;
+        public string whatanimal;
+
+        public override string Country { get; set; }
+        public override bool HideFromOtherAnimals { get; set; }
+        public override string Name { get; set; }
+        public override string WhatAnimal { get; set; }
+
+        public Pig(string country, bool hide, string name, string whatanimal, string food) : base(country, hide, name, whatanimal) { }
+
+        public override eFavouriteFood GetFavouriteFood()
+        {
+            return eFavouriteFood.Everything;
+
+        }
+
+        public override eClassificationAnimal GetClassificationAnimal()
+        {
+            return eClassificationAnimal.Omnivores;
+        }
+
+        public override string SayHello()
+        {
+            return "c";
+        }
+
+    }
+
+}
---
+++ lab7/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+using ClassLibrary1;
+
+class Program
+{
+    static void Main()
+    {
+        Assembly assembly = Assembly.Load("ClassLibrary1");
+        XElement root = new XElement("Classes");
+
+        foreach (Type type in assembly.GetTypes())
+        {
+            if (!type.IsClass || type.IsSubclassOf(typeof(Attribute)))
+                continue;
+
+            XElement classElement = new XElement("Class");
+            classElement.Add(new XAttribute("Name", type.Name));
+
+            var commentAttribute = type.GetCustomAttributes(typeof(CommentAttribute), false)
+                                       .FirstOrDefault() as CommentAttribute;
+            if (commentAttribute != null)
+            {
+                classElement.Add(new XElement("Comment", commentAttribute.Comment));
+            }
+
+            XElement propertiesElement = new XElement("Properties");
+            foreach (var prop in type.GetProperties())
+            {
+                propertiesElement.Add(new XElement("Property", prop.Name));
+            }
+            classElement.Add(propertiesElement);
+
+            XElement methodsElement = new XElement("Methods");
+            foreach (var method in type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly))
+            {
+                methodsElement.Add(new XElement("Method", method.Name));
+            }
+            classElement.Add(methodsElement);
+
+            root.Add(classElement);
+        }
+
+
+        XDocument document = new XDocument(root);
+        document.Save("ClassDiagram.xml");
+
+        Console.WriteLine("XML-документ сгенерирован: ClassDiagram.xml");
+    }
+}
